9c78f93e0d445aedf6030ac4f844d904f4b9f724
mateus <mateusmmo15@gmail.com>
2021-04-21 23:01:11 -0300

    fim do segundo dia de maratona

---
+++ src/controllers/SettingsController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm"
+import { SettingsRepository} from "../repositories/SettingsRepository";
+
+class SettingsController {
+    async create(request: Request, response:Response){
+        const settingsRepository = getCustomRepository(SettingsRepository);
+
+        const { chat, username } = request.body;
+    
+        const settings = settingsRepository.create({
+            chat,
+            username
+        });
+    
+        await settingsRepository.save(settings);
+    
+        return response.json(settings);
+    }
+}
+
+export { SettingsController }
